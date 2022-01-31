4601c7a19e45115a37389a611c1f1c92b116ab08
guptaShubham02 <shubhamgupta90970@gmail.com>
2022-01-31 13:41:33 +0530

    carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $( document ).ready(function(){
-   
+   $('.small-nav-page').click(function(){
+       $('.small-dropdown').slideToggle();
+       $('.small-arrow').toggleClass('right-rotate');
+   });
+
+   $('.carousel-animate').children().addClass('animate__animated');
+   $('.carousel-animate').children().addClass('animate__slideInUp');
 });
