119fc9f337079ec1ffed5c30ba1409759cdb6f4b
guptaShubham02 <shubhamgupta90970@gmail.com>
2022-02-08 14:26:05 +0530

    counter-half

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-$( document ).ready(function(){
-   $('.small-nav-page').click(function(){
-       $('.small-dropdown').slideToggle();
-       $('.small-arrow').toggleClass('right-rotate');
-   });
+$(document).ready(function () {
+  $('.small-nav-page').click(function () {
+    $('.small-dropdown').slideToggle();
+    $('.small-arrow').toggleClass('right-rotate');
+  });
 
-   $('.carousel-animate').children().addClass('animate__animated');
-   $('.carousel-animate').children().addClass('animate__slideInUp');
+  $('.carousel-animate').children().addClass('animate__animated animate__slideInUp');
+
+  //    Masonry init
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item, .grid-item-1',
+    columnWidth: 0,
+    // columnWidth: '.grid-sizer',
+    // percentPosition: true,
+    // gutter: '10px'
+  });
 });
