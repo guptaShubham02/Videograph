6c338de298f6264431f1bf21273caba7edbc1963
guptaShubham02 <shubhamgupta90970@gmail.com>
2022-02-17 22:21:37 +0530

    85%

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
 $(document).ready(function () {
-  $('.small-nav-page').click(function () {
-    $('.small-dropdown').slideToggle();
-    $('.small-arrow').toggleClass('right-rotate');
+  $(".small-nav-page").click(function () {
+    $(".small-dropdown").slideToggle();
+    $(".small-arrow").toggleClass("right-rotate");
   });
 
-  $('.carousel-animate').children().addClass('animate__animated animate__slideInUp');
+  $(".carousel-animate")
+    .children()
+    .addClass("animate__animated animate__slideInUp");
 
   //    Masonry init
-  $('.grid').masonry({
+  $(".grid").masonry({
     // options
-    itemSelector: '.grid-item, .grid-item-1',
+    itemSelector: ".grid-item, .grid-item-1",
     columnWidth: 0,
-    // columnWidth: '.grid-sizer',
-    // percentPosition: true,
-    // gutter: '10px'
+  });
+});
+
+// Owl Carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    responsiveClass: true,
+    nav: false,
+    responsive: {
+      992: {
+        items: 3,
+      },
+      768: {
+        items: 2,
+      },
+      320: {
+        items: 1,
+      },
+    },
+    dotsEach: true,
+    autoplay: true,
+    autoHeight: false,
+    smartSpeed: 1200
   });
 });
